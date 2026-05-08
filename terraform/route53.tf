cec891ee9b26c1595299c276059e9b9fd2f7c17d
Minh Huy <mhuy.dev@gmail.com>
2026-05-08 19:01:09 +0700

    fix route53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -24,19 +24,19 @@ resource "aws_acm_certificate" "wildcard" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.wildcard.domain_validation_options : dvo.resource_record_name => {
+    for dvo in aws_acm_certificate.wildcard.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
       record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
 
-  allow_overwrite = true
+  allow_overwrite = true 
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
   zone_id         = aws_route53_zone.main.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "wildcard" {
